ff6154b502aec4887ee31fe8878b43f80047e87b
TechDudie <73961295+TechDudie@users.noreply.github.com>
2022-03-25 10:05:51 -0500

    Update dnd.js

--- js/dnd.js
+++ js/dnd.js
@@ -1,44 +1,12 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-
-  function handleDragStart(e) {
-    this.style.opacity = '0.4';
-  }
-
-  function handleDragEnd(e) {
-    this.style.opacity = '1';
-
-    items.forEach(function (item) {
-      item.classList.remove('over');
-    });
-  }
-
-  function handleDragOver(e) {
-    if (e.preventDefault) {
-      e.preventDefault();
-    }
-
-    return false;
-  }
-
-  function handleDragEnter(e) {
-    this.classList.add('over');
-  }
-
-  function handleDragLeave(e) {
-    this.classList.remove('over');
-  }
-  
-  function handleDrop(e) {
-    e.stopPropagation(); // stops the browser from redirecting.
-    return false;
-  }
-  let items = document.querySelectorAll('.container .box');
-  items.forEach(function(item) {
-    item.addEventListener('dragstart', handleDragStart);
-    item.addEventListener('dragover', handleDragOver);
-    item.addEventListener('dragenter', handleDragEnter);
-    item.addEventListener('dragleave', handleDragLeave);
-    item.addEventListener('dragend', handleDragEnd);
-    item.addEventListener('drop', handleDrop);
-  });
-});
+window.onload = function() {
+  var box = document.getElementById('piece1');
+  box.addEventListener('touchmove', function(e) {
+    var touchLocation = e.targetTouches[0];
+    box.style.left = touchLocation.pageX + 'px';
+    box.style.top = touchLocation.pageY + 'px';
+  })
+  box.addEventListener('touchend', function(e) {
+    var x = parseInt(box.style.left);
+    var y = parseInt(box.style.top);
+  })
+}
